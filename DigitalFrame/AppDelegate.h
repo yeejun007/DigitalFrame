3653109fea93376f4af9500c7b7238a7145f4613
yeejun007 <dlwns007@gmail.com>
2020-07-20 15:53:40 +0900

    Initial Commit

---
+++ DigitalFrame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DigitalFrame
+//
+//  Created by 송이준 on 20/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DigitalFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DigitalFrame
+//
+//  Created by 송이준 on 20/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
