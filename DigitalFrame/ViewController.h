5c6694d763798d3a0f614698e62619f99c02e8b5
yeejun007 <dlwns007@gmail.com>
2020-07-22 16:27:38 +0900

    added UIImageView, UIButton, toggleAction func

--- DigitalFrame/ViewController.h
+++ DigitalFrame/ViewController.h
@@ -10,6 +10,11 @@
 
 @interface ViewController : UIViewController
 
+// 실제로 앱 상에서 눈에 보이는 상태를 관리하는것이 property임
+@property (nonatomic, strong) IBOutlet UIImageView *imgView;
+@property (nonatomic, strong) IBOutlet UIButton *toggleButton; 
 
+// 액션은 상태가 아니라 유저 행동에 대한 반응을 정의하는 것임
+-(IBAction)toggleAction: (id)sender;
 @end
 
--- DigitalFrame/ViewController.m
+++ DigitalFrame/ViewController.m
@@ -13,10 +13,41 @@
 @end
 
 @implementation ViewController
+// 헤더파일에서 정의한 상태 변수들을 동기화시켜준다
+@synthesize imgView, toggleButton;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSArray *cuteImages = [[NSArray alloc] initWithObjects:
+                           [UIImage imageNamed:@"1.jpg"],
+                           [UIImage imageNamed:@"2.jpg"],
+                           [UIImage imageNamed:@"3.jpg"],
+                           [UIImage imageNamed:@"4.jpg"],
+                           [UIImage imageNamed:@"5.jpg"],
+                           [UIImage imageNamed:@"6.jpg"],
+                           [UIImage imageNamed:@"7.jpg"],
+                           [UIImage imageNamed:@"8.jpg"],
+                           [UIImage imageNamed:@"9.jpg"],
+                           [UIImage imageNamed:@"10.jpg"],
+                           [UIImage imageNamed:@"11.jpg"],
+                           [UIImage imageNamed:@"12.jpg"],
+                           [UIImage imageNamed:@"13.jpg"],
+                           [UIImage imageNamed:@"14.jpg"],
+                           [UIImage imageNamed:@"15.jpg"],
+                           nil];
+    imgView.animationImages = cuteImages;
+    imgView.animationDuration = 15.0;
+//    imgView.startAnimating;
+}
+
+-(IBAction)toggleAction: (id)sender {
+    if([imgView isAnimating]) {
+        [imgView stopAnimating];
+        [toggleButton setTitle: @"Start" forState:UIControlStateNormal];
+    }else {
+        [imgView startAnimating];
+        [toggleButton setTitle: @"Stop" forState:UIControlStateNormal];
+    }
 }
 
 
